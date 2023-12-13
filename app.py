e6bb94024aabf1ab409b0ed1d4e16ee8cbd5d714
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2023-12-13 09:32:47 +0530

    Status label added

--- app.py
+++ app.py
@@ -197,6 +197,13 @@ class VideoDownloder :
                                                          bg_color = "grey", fg_color = "white", text_color = "red"  )
                         vid_count_val.place_configure( x = 100, y = 430, anchor = "nw" )
 
+                        # Status Label
+                        status_chk = ctk.CTkLabel( master = frame, 
+                                                    text = details["status"], text_font = ("Georgia", 18),
+                                                     width = 360, height = 230, corner_radius = 15,
+                                                      bg_color = "grey", fg_color = "white", text_color = "red"  )
+                        status_chk.place_configure( x = 500, y = 150, anchor = "nw" )
+
         except :
             showerror( title = "Invalid Link", message = "Invalid Link Passed!!")
 
